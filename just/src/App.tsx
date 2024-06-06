14d0435d2f5ef4d818de4386e5edbcbd61461d41
wendellast <wendelalves01a@hotmail.com>
2024-06-06 17:36:59 -0300

    feat: add-new component

--- just/src/App.tsx
+++ just/src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+
 import "./app.css"
 const App = () =>{
-  return <div>WENDELLAST</div>
+  const [showGame, setShowGame] = useState<boolean>(false)
+  const [repos, setRepos ] = useState(["repos1"])
+
+  const handleStartClick = () =>{
+    setShowGame(true)
+ }
+  return <main>
+    {!showGame && (
+      <>
+      <h1>Welcome to Just-Wars</h1>
+      <h2>A developer-game</h2>
+      {repos.length === 0 ? (<p>loading-game</p> ):(<button type="submit" onClick={handleStartClick} >Start</button>)}
+      </>
+    )}
+
+    {showGame &&(
+      <>
+        <h1>Game</h1>
+      </>
+    )}
+  </main>
 }
 
 export default App
